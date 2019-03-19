60db64e6c83474086de467066602f9cbb0532c9d
Anooj Shah <thisisanooj@gmail.com>
2019-03-19 12:02:27 -0500

    update servo.ino

--- servo.ino
+++ servo.ino
@@ -11,7 +11,7 @@ int val = 0;
 void setup() {
   Serial.begin(9600);
   myservo.attach(8);
-  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees
       // in steps of 1 degree
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);                       // waits 15ms for the servo to reach the position
@@ -23,29 +23,29 @@ void loop() {
   if(Serial.available() > 0) {
     userInput = Serial.read();
     if(userInput == 'g') {
-      for (pos = 90; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      for (pos = 90; pos <= 180; pos += 1) { // goes from 90 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
     }
     if(userInput == 'h') {
-     for (pos = 180; pos >= 90; pos -= 1) { // goes from 180 degrees to 0 degrees
+     for (pos = 180; pos >= 90; pos -= 1) { // goes from 180 degrees to 90 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
     }
     if(userInput == 'i') {
-      for (pos = 90; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+      for (pos = 90; pos >= 0; pos -= 1) { 
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);             
+    delay(15);                       
   }
     }
     if(userInput == 'j') {
-     for (pos = 0; pos <= 90; pos += 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+     for (pos = 0; pos <= 90; pos += 1) { 
+    myservo.write(pos);              
+    delay(15);                       
   }
     }
     
